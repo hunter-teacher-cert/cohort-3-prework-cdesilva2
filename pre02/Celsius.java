cec6b6b4dd1af5bbc19bf5bb3a87acf6cb4808e1
Cdesilva2 <97003729+cdesilva2@users.noreply.github.com>
2022-01-03 19:05:10 +0000

    did not finish chapter 3 prblem 2

---
+++ pre02/Celsius.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Converts Celsius to Fahrenheit
+ */
+public class Celsius {
+
+    public static void main(String[] args) {
+        double cel;
+        double far;
+        final double fardiv = (9/5);
+        Scanner in = new Scanner(System.in);
+
+        // prompt the user and get the value
+        System.out.println("Celsius to Fahrenheit");
+        System.out.print("Enter a temperature in Celsius: ");
+        cel = in.nextDouble();
+
+        // convert and output the result
+        far = (cel * fardiv) + 32;
+        System.out.println(cel, "C = ", far, "F = ");
+    }
+}
